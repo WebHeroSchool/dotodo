6595e79d6f6b910f099aa1f1205c89e732f39b1e
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-08-06 14:21:24 +0400

    Запрос данных с сервера

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,97 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './About.module.css';
+import { Octokit } from '@octokit/rest';
 
-const About = () => <h1 className={styles.title}>About</h1>;
+const octokit = new Octokit();
+
+class About extends React.Component {
+  state = {
+    isLoading: true,
+    isError: false,
+    error: '',
+    repoList: [],
+    info: []
+  }
+
+  componentDidMount() {
+    octokit.repos.listForUser({
+      username: 'tytytyw'
+    }).then(({ data }) => {
+      this.setState({
+        repoList: data,
+        isLoading: false
+      })
+    })
+    .catch(error => {
+      this.setState({
+        isLoading: false,
+        isError: true,
+        error: error.message
+      })
+    })
+    
+   octokit.users.getByUsername({
+      username: 'tytytyw'
+        }).then(({ data }) => {
+          this.setState({
+            info: data,
+        });
+    })
+    .catch(error => {
+      this.setState({
+        bioIsLoading: false,
+        isError: true,
+        error: error.message
+      })
+    })
+  };
+
+  render() {
+    const { isLoading, isError, error, repoList, info } = this.state;
+    return (
+      isLoading ? <CircularProgress /> :
+        <div className={styles.wrap}>
+
+          {!isLoading && !isError &&
+            <div className={styles.bio}>
+              <img src={info.avatar_url} alt='avatar GitHub'></img>
+              <h4 className={styles.content}>
+                <div>
+                  name: {info.name}
+                </div>
+                <a className={styles.link} href={info.html_url}>
+                    GitHub: {info.login}
+                </a>
+              </h4>
+            </div>
+          }
+
+          {!isLoading && !isError &&
+            <div className={styles.title}>
+              Мои репозитрии:
+              <ol className={styles.repo_list}>
+                {repoList.map(repo => (
+                <li key={repo.id}>
+                  <a
+                  className={styles.link}
+                  href={repo.html_url}>
+                    {repo.name}
+                  </a>
+                </li>))}
+              </ol>
+            </div>
+          }
+
+          {isError &&
+            <div className={styles.error}>
+              Ошибка: {error}
+            </div>
+          }
+
+      </div>
+    );
+  }
+}
 
 export default About;
