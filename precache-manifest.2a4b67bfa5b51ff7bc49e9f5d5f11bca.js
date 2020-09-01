0bd66f242435fe05efa8e0e6c0dd14165e028442
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2020-09-01 13:06:09 +0400

    Updates

---
+++ precache-manifest.2a4b67bfa5b51ff7bc49e9f5d5f11bca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b4b0fca2dc8897d8ed9383573bbe79",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ad174ae8a1633f3253c0",
+    "url": "/react-gh-pages/static/css/main.6ff3be08.chunk.css"
+  },
+  {
+    "revision": "ad649bc2b427a86937a0",
+    "url": "/react-gh-pages/static/js/2.2e861e8c.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/react-gh-pages/static/js/2.2e861e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad174ae8a1633f3253c0",
+    "url": "/react-gh-pages/static/js/main.1af4b81f.chunk.js"
+  },
+  {
+    "revision": "75ec4037069d80e37c37",
+    "url": "/react-gh-pages/static/js/runtime-main.3d3ec97d.js"
+  },
+  {
+    "revision": "69c703b21776155b4bf13adbbb72e5a5",
+    "url": "/react-gh-pages/static/media/Montserrat-Bold.69c703b2.ttf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/react-gh-pages/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "5906fb82e31864c9b531898bb3c97d98",
+    "url": "/react-gh-pages/static/media/Montserrat-Regular.5906fb82.ttf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/react-gh-pages/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "261dd5d825e501e6d8bbcb8bb1c87155",
+    "url": "/react-gh-pages/static/media/delete.261dd5d8.svg"
+  }
+]);
